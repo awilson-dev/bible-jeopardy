53fecc257c4bde25d430d0fee47d1ec72872ae9c
Allen Wilson <a_m_wilson@icloud.com>
2024-01-11 11:24:58 -0500

    Initial Commit

---
+++ Bible Jeopardy/Bible_JeopardyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bible_JeopardyApp.swift
+//  Bible Jeopardy
+//
+//  Created by Allen Wilson on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Bible_JeopardyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
