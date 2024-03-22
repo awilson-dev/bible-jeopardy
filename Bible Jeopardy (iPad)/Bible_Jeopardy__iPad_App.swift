6b7d5ddeda217231f39f35935115ff216ef2f87a
awilson-dev <awilson-dev@tech-center.com>
2024-03-22 13:16:24 -0400

    Add iOS app target

---
+++ Bible Jeopardy (iPad)/Bible_Jeopardy__iPad_App.swift
@@ -0,0 +1,17 @@
+//
+//  Bible_Jeopardy__iPad_App.swift
+//  Bible Jeopardy (iPad)
+//
+//  Created by Allen Wilson on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Bible_Jeopardy__iPad_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
