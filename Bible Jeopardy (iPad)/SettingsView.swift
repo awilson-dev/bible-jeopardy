7248f28c0c9f21f7de033aa32720e96d7894c990
awilson-dev <awilson-dev@tech-center.com>
2024-05-07 16:31:33 -0400

    Add settings view

---
+++ Bible Jeopardy (iPad)/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Bible Jeopardy (iPad)
+//
+//  Created by Allen Wilson on 5/7/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
